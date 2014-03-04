b70668118d4bc7554601f2139b989c323385dbff
NESSIM <NESSIM@NESSIM-PC.connectify>
2014-03-04 12:04:15 +0100

    interface offre

--- src/gc/gui/RechecheOffre.java
+++ src/gc/gui/RechecheOffre.java
@@ -6,18 +6,22 @@
 
 package gc.gui;
 
+import gc.dao.OffreDAO;
+import gc.entities.Offre;
+import java.util.List;
+
 /**
  *
  * @author NESSIM
  */
-public class RechecheOffre extends javax.swing.JFrame {
-private final ListSalleFetesModel modele=new ListSalleFetesModel();
-    /**
-     * Creates new form RechercheSalleDesFetes
-     */
-    public RechecheOffre() {
+public class RechecheOffre extends javax.swing.JFrame {    
+private ListSalleFetesModel modele=new ListSalleFetesModel();
+private ListSalleFetesModel recherche;
+public RechecheOffre() {
         initComponents();
     }
+    /**
+     * Creates new form RechercheSalleDesFetes
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -31,10 +35,10 @@ private final ListSalleFetesModel modele=new ListSalleFetesModel();
         jScrollPane1 = new javax.swing.JScrollPane();
         Tab_salle_fetes = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jComboBox1 = new javax.swing.JComboBox();
-        jTextField3 = new javax.swing.JTextField();
+        TxtNom = new javax.swing.JTextField();
+        TxtPrix = new javax.swing.JTextField();
+        Combotype = new javax.swing.JComboBox();
+        TextLieux = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
         jLabel2 = new javax.swing.JLabel();
@@ -44,18 +48,26 @@ private final ListSalleFetesModel modele=new ListSalleFetesModel();
         jLabel6 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
+            }
+        });
 
         Tab_salle_fetes.setFont(new java.awt.Font("Times New Roman", 0, 14)); // NOI18N
         Tab_salle_fetes.setModel(modele);
         jScrollPane1.setViewportView(Tab_salle_fetes);
 
         jButton1.setText("rechercher");
-
-        jComboBox1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
-
-        jTextField3.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jTextField3ActionPerformed(evt);
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        TextLieux.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TextLieuxActionPerformed(evt);
             }
         });
 
@@ -96,21 +108,21 @@ private final ListSalleFetesModel modele=new ListSalleFetesModel();
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(TextLieux, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(TxtNom, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addGap(103, 103, 103)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
-                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(TxtPrix, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(26, 26, 26)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addComponent(Combotype, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addGap(74, 74, 74))
@@ -135,15 +147,15 @@ private final ListSalleFetesModel modele=new ListSalleFetesModel();
                 .addComponent(jLabel1)
                 .addGap(34, 34, 34)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TxtPrix, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(TxtNom, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel3)
                     .addComponent(jLabel5))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 24, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                        .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(TextLieux, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(Combotype, javax.swing.GroupLayout.PREFERRED_SIZE, 23, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addComponent(jButton1)
                         .addComponent(jLabel6))
                     .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING))
@@ -155,12 +167,42 @@ private final ListSalleFetesModel modele=new ListSalleFetesModel();
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 202, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
+        jLabel6.getAccessibleContext().setAccessibleName("Lieu");
+
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jTextField3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField3ActionPerformed
+    private void TextLieuxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextLieuxActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jTextField3ActionPerformed
+    }//GEN-LAST:event_TextLieuxActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+String nom=TxtNom.getText();
+Double prix=0.0;
+if( !"".equals(TxtPrix.getText()))
+{
+    prix=Double.parseDouble(TxtPrix.getText());
+} 
+String lieu=TextLieux.getText();
+String type=Combotype.getSelectedItem().toString();
+       
+      recherche=new ListSalleFetesModel(nom,prix,lieu,type);
+        Tab_salle_fetes.setModel(recherche);
+        
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+
+        // TODO add your handling code here:
+        OffreDAO offredao=new OffreDAO();
+        List<Offre> resultat=offredao.DisplayType();
+        for(Offre o:resultat)
+        {
+            Combotype.addItem(o.getType());
+        }
+        
+    }//GEN-LAST:event_formWindowOpened
 
     /**
      * @param args the command line arguments
@@ -199,9 +241,12 @@ private final ListSalleFetesModel modele=new ListSalleFetesModel();
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox Combotype;
     private javax.swing.JTable Tab_salle_fetes;
+    private javax.swing.JTextField TextLieux;
+    private javax.swing.JTextField TxtNom;
+    private javax.swing.JTextField TxtPrix;
     private javax.swing.JButton jButton1;
-    private javax.swing.JComboBox jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -210,8 +255,5 @@ private final ListSalleFetesModel modele=new ListSalleFetesModel();
     private javax.swing.JLabel jLabel6;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
-    private javax.swing.JTextField jTextField1;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
     // End of variables declaration//GEN-END:variables
 }
